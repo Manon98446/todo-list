b0520efb06b24f0b3c8c97f6287391788e411225
Manon <manon.vdm@hotmail.com>
2023-07-14 13:52:26 +0200

    localstorage works

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import React,{ useState } from 'react'
+import React,{ useEffect, useState } from 'react'
 import AddTodo from './Components/addTodo';
 import Header from './Components/header';
 import TodoList from './Components/todoList';
+import {v4 as uuidv4} from "uuid";
 
 function App() {
-  const initialTodos = [
+
+  const LSKEY = "MyTodoApp"; //clef de notre application
+  const initTodos = [
     {
-        id: "0",
+        id: uuidv4(),
         name: 'Apprendre React',
         done: false
     },
     {
-        id:'1',
+        id:uuidv4(),
         name: 'Faire la vaisselle',
         done: true
     },
     {
-        id:'2',
+        id:uuidv4(),
         name: 'courir',
         done: true
     }
   ];
-  const [todos, setTodos] = useState(initialTodos); //state permet de modifier le DOM en fonction du state, ce qu'une simple variable (let) ne permet pas, le array pourrait etre modifié par let mais ce ne serait pas affiché sur l'interface de l'utilisateur
-  
+  const [todos, setTodos] = useState([]); //state permet de modifier le DOM en fonction du state, ce qu'une simple variable (let) ne permet pas, le array pourrait etre modifié par let mais ce ne serait pas affiché sur l'interface de l'utilisateur
+  const [firstLoad, setFirstLoad] = useState(true);
   const changeTodoStatus = (id) => {
     const newTodos = todos.map((todo) => {
         if(todo.id === id){
@@ -35,7 +38,7 @@ function App() {
 
   const AddNewTodo =(newtTodoName) => {
     const newTodo = {
-      id: todos.length.toString(),
+      id: uuidv4(),
       name: newtTodoName,
       done: false
     }
@@ -43,6 +46,20 @@ function App() {
     newTodos.push(newTodo);
     setTodos(newTodos);
   }
+
+  useEffect(() => {
+    if(!firstLoad){
+      window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos))
+    } else {
+      setFirstLoad(false);
+    }
+  } , [todos, firstLoad]); //lorsque [todos] a un changement, la fonction pour localstorage est appelée
+
+  useEffect(() => {
+    const initialTodos = JSON.parse(window.localStorage.getItem(LSKEY + ".todos"))
+    setTodos(initialTodos)
+  }, []);
+
   return (
     <main className="container">
       <Header />
